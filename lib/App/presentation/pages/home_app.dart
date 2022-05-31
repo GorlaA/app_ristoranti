801315092ccae872ed942ce5cd92b765427ae541
Gabriele Groppo <lelegroppo.gg@gmail.com>
2022-05-31 22:42:58 +0200

    Modifiche app home
    > Modifica widget lista ristoranti:
    - Da adesso widget di tipo StateFulWidget
    - Aggiunta costruttore per passaggio nome lista e lista di ristoranti.
    - Cambio nome in ListaRistoranti
    - Aggiunta Stato _ListaRistotantiState
    - Aggiunta visualizzazione titolo lista
    > Rimozione metodi vecchi (Chiedi a Lele se ne serviva qualcuno rimosso backup sul mio Notion).

--- lib/App/presentation/pages/home_app.dart
+++ lib/App/presentation/pages/home_app.dart
@@ -4,26 +4,33 @@ import 'package:app_ristoranti/App/presentation/widgets/container_ristorante.dar
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main(){
-  runApp(new Home_app_page());
+  //Oggetto firebase
+  runApp(Home_app_page("Gabriele"));
 }
 
 class Home_app_page extends StatelessWidget{
+  String nomeUtente = "";
+  List<Ristorante> ristoranti = [];
+  Home_app_page(this.nomeUtente, {Key? key}) : super(key: key){
+    ristoranti = [
+      Ristorante("Marcellino", "assets/images/Marcellino.jpg", 1),
+      Ristorante("Marcellino", "assets/images/Marcellino.jpg", 2),
+      Ristorante("Marcellino", "assets/images/Marcellino.jpg", 3),
+      Ristorante("Marcellino", "assets/images/Marcellino.jpg", 4),
+      Ristorante("Marcellino", "assets/images/Marcellino.jpg", 5),
+    ];
+  }
   @override
   Widget build(BuildContext context){
     return MaterialApp(
       title: 'App',
       home: Scaffold(
         bottomNavigationBar: Bottom_Navigation_Bar(),
-        /*appBar: PreferredSize(
-          preferredSize: Size.fromHeight(100.0),
-          child:
-        ),*/
         body: CustomScrollView(
           slivers: <Widget>[//lista di figli
-            Silver_App_Bar_Home(),
-            //Silver_List(),
-            //Sliver_App_Bar_Home(),
-            Sliver_List(),
+            Sliver_App_Bar_Home(nomeUtente),
+            ListaRistoranti("In evidenza", ristoranti),
+            ListaRistoranti("Nelle vicinanze", ristoranti)
           ],
         ),
       ),

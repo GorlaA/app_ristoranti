1b6b1149cc57349b0e0ead48bc1ef5e4a876922c
Gabriele Groppo <lelegroppo.gg@gmail.com>
2022-06-12 20:18:28 +0200

    Commento //Cloud_firestore import

--- lib/App/presentation/pages/map_page.dart
+++ lib/App/presentation/pages/map_page.dart
@@ -2,7 +2,7 @@ import 'package:app_ristoranti/App/presentation/pages/profile_page.dart';
 import 'package:flutter/material.dart';
 import 'package:app_ristoranti/App/presentation/bloc/Elementi_Home.dart';
 import 'package:app_ristoranti/App/presentation/widgets/container_ristorante.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
 
 class MapPage extends StatelessWidget{

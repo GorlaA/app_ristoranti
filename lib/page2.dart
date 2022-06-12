8cfe40eee54ff7f1c75790394ee8c36ede1bb9fa
Danilo <a@gmai.com>
2022-06-12 16:50:38 +0200

    Aggiunti puntatori sulla mappa

--- lib/page2.dart
+++ lib/page2.dart
@@ -2,10 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-
-void main() => runApp(page2());
-
-class page2 extends StatelessWidget {
+/*class page2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,6 +11,7 @@ class page2 extends StatelessWidget {
     );
   }
 }
+*/
 
 class MapSample extends StatefulWidget {
   @override
@@ -21,50 +19,48 @@ class MapSample extends StatefulWidget {
 }
 
 class MapSampleState extends State<MapSample> {
+  Set<Marker>  _markers = {};
   Completer<GoogleMapController> _controller = Completer();
 
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(45.49060628897456, 8.958427513477101),
-    zoom: 14.4746,
+    zoom: 15,
   );
 
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(45.382881765846086, 8.915598286593585),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+  void _onMapCreated(GoogleMapController controller){
+    _controller.complete(controller);
+    setState((){
+      for (var i = 0; i < 5; i++) {
+        _markers.add(
+            Marker(
+              markerId: MarkerId('id-'+i.toString()),
+              position: LatLng(45.49060628897456+i, 8.958427513477101),
+              infoWindow: InfoWindow(
+                  title: 'Marcellino'+i.toString(),
+                  snippet: 'paninaro'
+              ),
+            )
+        );
+      }
+    });
+  }
+
+  /* static final CameraPosition _kLake = CameraPosition(
+ *   //bearing: 192.8334901395799, //orienta la mappa a nord
+ *    target: LatLng(45.382881765846086, 8.915598286593585),
+ *   //tilt: 59.440717697143555, //zoom 3D
+ *  zoom: 19.151926040649414);
+ */
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: GoogleMap(
-        mapType: MapType.hybrid,
+        onMapCreated: _onMapCreated,
+        markers: _markers,
+        mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('Vai da gorla'),
-        icon: Icon(Icons.directions_boat),
       ),
     );
   }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
-  }
 }
-
-
-/*
-
-import 'dart:async';
-
-import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-
-
- */

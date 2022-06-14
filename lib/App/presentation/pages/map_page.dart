3821d9f8a0f93be6952bd4072106ce51be2d4916
Gabriele Groppo <lelegroppo.gg@gmail.com>
2022-06-14 14:10:59 +0200

    Aggiunta container ristorante nella mappa
    Aggiunta event handling by Provider per:
    Mostrare il container e nasconderlo

--- lib/App/presentation/pages/map_page.dart
+++ lib/App/presentation/pages/map_page.dart
@@ -8,10 +8,46 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 //import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
 
+class MapProvider extends ChangeNotifier{
+  bool visibleConRist = false;
+  MapProvider();
+
+  void changeContRistState(visible){
+    visibleConRist = visible;
+    notifyListeners();
+  }
+  get currentContState => visibleConRist;
+}
+
 class MapPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return ColoredSafeArea(color: Color.fromARGB(255, 250, 182, 80),child: Stack(children: [Expanded(child: MapSample()), TopBarPage("Mappa")],),);
+    return ColoredSafeArea(
+      color: Color.fromARGB(255, 250, 182, 80),
+      child: MultiProvider(
+          providers: [
+            ChangeNotifierProvider(create: (context) => MapProvider(),)
+          ],
+          child: Stack(
+              children: [
+                Expanded(
+                    child: MapSample()
+                ),
+                TopBarPage("Mappa"),
+                Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children:[
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        child: MostraRistorantePing(),
+                      ),
+                      Padding(padding: EdgeInsets.only(bottom: 10))
+                    ]
+                ),
+              ]
+          ),
+      ),
+    );
   }
 }
 
@@ -37,9 +73,10 @@ class MapSampleState extends State<MapSample> {
             Marker(
               markerId: MarkerId('id-'+i.toString()),
               position: LatLng(45.49060628897456+i, 8.958427513477101),
+              consumeTapEvents: true,
+              onTap:(){Provider.of<MapProvider>(context, listen: false).changeContRistState(true);},
               infoWindow: InfoWindow(
                   title: 'Marcellino'+i.toString(),
-                  snippet: 'paninaro'
               ),
             )
         );
@@ -56,14 +93,28 @@ class MapSampleState extends State<MapSample> {
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
+    return Scaffold(
       body: GoogleMap(
         onMapCreated: _onMapCreated,
         markers: _markers,
         mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex,
         zoomControlsEnabled: false,
+        onTap:(LatLng latLng){Provider.of<MapProvider>(context, listen: false).changeContRistState(false);},
       ),
     );
   }
 }
+
+class MostraRistorantePing extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    bool visibile = Provider.of<MapProvider>(context, listen: true).currentContState;
+    if(visibile)
+      return Container_Ristorante(RistoTesting().getRistoranteTest);
+    else
+      return Container();
+  }
+}
+
+
